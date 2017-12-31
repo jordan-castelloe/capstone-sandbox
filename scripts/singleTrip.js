2f2ca0f94988cb269f4e3673908da61f8c262569
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-12-31 15:36:15 -0600

    changed zoom on map

--- scripts/singleTrip.js
+++ scripts/singleTrip.js
@@ -55,44 +55,17 @@ function showMap(latlngObject) {
     console.log("show map function fired!");
     let mapContainer = document.getElementById("single-trip-map-container"); 
     map = new google.maps.Map(mapContainer, {
-        zoom: 6,
+        zoom: 8,
         center: latlngObject,
         zoomControl: true
     });
 }
 
-  function createMarkers(locationObject) {
+function createMarkers(locationObject) {
     console.log("create marker function fired!");
-     let marker = new google.maps.Marker({
-       position: locationObject[0].geometry.location,
-       map: map,
-       title: "title"
-     });
-  }
-
-
-//   document.getElementById("submit").addEventListener("click", function() {
-//     geocodePlaceId(geocoder, map, infowindow);
-//   });
-
-    // function geocodePlaceId(geocoder, map, infowindow) {
-    //   var placeId = document.getElementById("place-id").value;
-    //   geocoder.geocode({ placeId: placeId }, function(results, status) {
-    //     if (status === "OK") {
-    //       if (results[0]) {
-    //         map.setZoom(11);
-    //         map.setCenter(results[0].geometry.location);
-    //         var marker = new google.maps.Marker({
-    //           map: map,
-    //           position: results[0].geometry.location
-    //         });
-    //         infowindow.setContent(results[0].formatted_address);
-    //         infowindow.open(map, marker);
-    //       } else {
-    //         window.alert("No results found");
-    //       }
-    //     } else {
-    //       window.alert("Geocoder failed due to: " + status);
-    //     }
-    //   });
-    // }
+    let marker = new google.maps.Marker({
+        position: locationObject[0].geometry.location,
+        map: map,
+        title: "title"
+    });
+}
