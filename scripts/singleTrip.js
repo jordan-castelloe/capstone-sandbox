854990fd4a77e9a1befe94628bfec6abfa954769
Jordan Castelloe <jordan.castelloe@gmail.com>
2017-12-31 11:36:18 -0600

    map centers to trip

--- scripts/singleTrip.js
+++ scripts/singleTrip.js
@@ -8,7 +8,8 @@ const geocoder = new google.maps.Geocoder();
 
 module.exports.loadTrip = function(tripName){
     let tripIDs = getTripData(); // pass in argument of trip name- for now it's just places Array
-    getLatLngs(tripIDs); 
+    getMapCenter(tripIDs[0]);
+    // getLatLngs(tripIDs); 
 };
 
 // this would accept an argument of trip name
@@ -18,38 +19,48 @@ function getTripData(){
     return tripData;
 }
 
+function getMapCenter(firstId){
+    geocoder.geocode({ placeId: firstId }, mapCenterCallback);
+}
+
+function mapCenterCallback(results, status){
+    let center = results[0].geometry.location;
+    showMap(center);
+}
+
 // convert all place ids to lat lng objects
 function getLatLngs(placeIdArray){
     for (let i = 0; i < placeIdArray.length; i++){
-        let placeId = placeIdArray[i];
-        geocoder.geocode({ placeId: placeId }, geocoderCallback);
-    }  
+        geocoder.geocode({ placeId: placeIdArray[i] }, geocoderCallback);
+    }
 }
+
+
 
 function geocoderCallback(results, status){
-    console.log("this should be the results",results);
-    console.log("this is the status", status);
+    let resultsArray = [];
+    resultsArray.push(results);
+    return resultsArray;
+    // console.log("this is a single result", results); // this is a single result (it looks like an array??)
+    // console.log("this is the type of result", typeof results); // but it says it's an object??
+    // console.log("this should be an array of the results", resultsArray); //wtf is going on
+    // console.log("this should be a single result pulled from the array and it SHOULD LOOK LIKE AN OBJECT", resultsArray[0][0]); //this works
+    // console.log("this should be a single result latlng object", resultsArray[0][0].geometry.location); // this works 
+    
 }
 
 
-function getMapCenter(){
-// figure out where to center the map? pick a lat lng??
+function showMap(latlngObject) {
+    console.log("show map function fired!");
+    let mapContainer = document.getElementById("single-trip-map-container"); 
+    map = new google.maps.Map(mapContainer, {
+        zoom: 6,
+        center: latlngObject,
+        zoomControl: true
+    });
 }
 
-function showMap(lat, long) {
-  let mapContainer = document.getElementById("single-trip-map-container"); 
-  map = new google.maps.Map(mapContainer, {
-    zoom: 6,
-    center: { lat: lat, lng: long }, // can be a latlng object
-    zoomControl: true
-  });
-}
 
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
-    center: { lat: 40.72, lng: -73.96 }
-  });
 
   function createMarkers(tripData) {
     //  var marker = new google.maps.Marker({
@@ -62,7 +73,7 @@ function initMap() {
 //   document.getElementById("submit").addEventListener("click", function() {
 //     geocodePlaceId(geocoder, map, infowindow);
 //   });
-}
+
     // function geocodePlaceId(geocoder, map, infowindow) {
     //   var placeId = document.getElementById("place-id").value;
     //   geocoder.geocode({ placeId: placeId }, function(results, status) {
