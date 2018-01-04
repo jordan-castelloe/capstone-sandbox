fc4ccf2c5c17a490bfdce81e0d7bd310b910c150
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-01-04 06:20:48 -0600

    changed trip description form

--- scripts/tripBuilderView.js
+++ scripts/tripBuilderView.js
@@ -2,7 +2,7 @@
 
 // prints into 'your trip' section under 'create trip' when you add something to your trip from the search results
 module.exports.printTrip = function(resultsDiv){
-    let tripContainer = $(".trip-container");
+    let tripContainer = $(".my-trip");
     let locationCard = $(resultsDiv).clone();
     locationCard.addClass("location-card");
     let removeButton = $("<button>", {class: "btn btn-secondary", id: "removeFromTrip"}).text("Remove From Trip");
